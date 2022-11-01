8d69e9af76e145d9d72a3177a9c443d553a9f6d5
Sold-T <thomas.soldwedel@gmail.com>
2022-11-01 10:36:23 +0100

    bug fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,18 +3,21 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div id ="about-id" className="about-section">
+    <div id="about-id" className="about-section">
       <h1>Wer sind TrioMio?</h1>
       <div className="about-flex">
         <p>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
           sed diam voluptua. At vero eos et accusam et justo duo dolores et ea.
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr
         </p>
         <p>
           Ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
           sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
           dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr
         </p>
       </div>
     </div>
